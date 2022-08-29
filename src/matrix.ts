87fefe3d326546b92dd76cea771c138d7f121bc4
Marius Röhm <marius.roehm@gmail.com>
2022-08-29 11:15:46 +0200

    import von übung

---
+++ src/matrix.ts
@@ -0,0 +1,244 @@
+import Vector from './vector';
+
+/**
+ * Class representing a 4x4 Matrix
+ */
+export default class Matrix {
+
+  /**
+   * Data representing the matrix values
+   */
+  data: Float32Array;
+
+  /**
+   * Constructor of the matrix. Expects an array in row-major layout. Saves the data as column major internally.
+   * @param mat Matrix values row major
+   */
+  constructor(mat: Array<number>) {
+    this.data = new Float32Array(16);
+    for (let row = 0; row < 4; row++) {
+      for (let col = 0; col < 4; col++) {
+        this.data[row * 4 + col] = mat[col * 4 + row];
+      }
+    }
+  }
+
+  /**
+   * Returns the value of the matrix at position row, col
+   * @param row The value's row
+   * @param col The value's column
+   * @return The requested value
+   */
+  getVal(row: number, col: number): number {
+    return this.data[col * 4 + row];
+  }
+
+  /**
+   * Sets the value of the matrix at position row, col
+   * @param row The value's row
+   * @param val The value to set to
+   * @param col The value's column
+   */
+  setVal(row: number, col: number, val: number) {
+    this.data[col * 4 + row] = val;
+  }
+
+  /**
+   * Returns a matrix that represents a translation
+   * @param translation The translation vector that shall be expressed by the matrix
+   * @return The resulting translation matrix
+   */
+  static translation(translation: Vector): Matrix {
+    return new Matrix([
+      1, 0, 0, translation.x,
+      0, 1, 0, translation.y,
+      0, 0, 1, translation.z,
+      0, 0, 0, 1
+    ]);
+  }
+
+  /**
+   * Returns a matrix that represents a rotation. The rotation axis is either the x, y or z axis (either x, y, z is 1).
+   * @param axis The axis to rotate around
+   * @param angle The angle to rotate
+   * @return The resulting rotation matrix
+   */
+  static rotation(axis: Vector, angle: number): Matrix {
+    // TODO
+    if (axis.x == 1) {
+      return new Matrix([
+        1, 0, 0, 0,
+        0, Math.cos(angle), -Math.sin(angle), 0,
+        0, Math.sin(angle), Math.cos(angle), 0,
+        0, 0, 0, 1
+      ]);
+    }
+    else if (axis.y == 1) {
+      return new Matrix([
+        Math.cos(angle), 0, Math.sin(angle), 0,
+        0, 1, 0, 0,
+        -Math.sin(angle), 0, Math.cos(angle), 0,
+        0, 0, 0, 1
+      ]);
+    }
+    else if (axis.z == 1) {
+      return new Matrix([
+        Math.cos(angle), -Math.sin(angle), 0, 0,
+        Math.sin(angle), Math.cos(angle), 0, 0,
+        0, 0, 1, 0,
+        0, 0, 0, 1
+      ]);
+    }
+    else{
+      return Matrix.identity();
+    }
+  }
+
+  /**
+   * Returns a matrix that represents a scaling
+   * @param scale The amount to scale in each direction
+   * @return The resulting scaling matrix
+   */
+  static scaling(scale: Vector): Matrix {
+    return new Matrix([
+      scale.x, 0, 0, 0,
+      0, scale.y, 0, 0,
+      0, 0, scale.z, 0,
+      0, 0, 0, 1
+    ]);
+  }
+
+  /**
+   * Constructs a lookat matrix
+   * @param eye The position of the viewer
+   * @param center The position to look at
+   * @param up The up direction
+   * @return The resulting lookat matrix
+   */
+  static lookat(eye: Vector, center: Vector, up: Vector): Matrix {
+    let f = center.sub(eye).div((center.sub(eye)).length).normalize();
+    let s = f.cross(up).normalize();
+    let u = s.cross(f).normalize();
+
+    return new Matrix([
+        s.x, s.y, s.z, 0,
+        u.x, u.y, u.z, 0,
+        -f.x, -f.y, -f.z, 0,
+        0, 0, 0, 1
+    ]).mul(new Matrix([
+        1, 0, 0, -eye.x,
+        0, 1, 0, -eye.y,
+        0, 0, 1, -eye.z,
+        0, 0, 0, 1
+    ]));
+  }
+
+  /**
+   * Constructs a new matrix that represents a projection normalisation transformation
+   * @param left Camera-space left value of lower near point
+   * @param right Camera-space right value of upper right far point
+   * @param bottom Camera-space bottom value of lower lower near point
+   * @param top Camera-space top value of upper right far point
+   * @param near Camera-space near value of lower lower near point
+   * @param far Camera-space far value of upper right far point
+   * @return The rotation matrix
+   */
+  static frustum(left: number, right: number, bottom: number, top: number, near: number, far: number): Matrix {
+    return new Matrix([
+      2 * near / (right - left), 0, (right + left) / (right - left), 0,
+      0, 2 * near / (top - bottom), (top + bottom) / (top - bottom), 0,
+      0, 0, -(far + near) / (far - near), -2 * far * near / (far - near),
+      0, 0, -1, 0
+    ]);
+  }
+
+  /**
+   * Constructs a new matrix that represents a projection normalisation transformation.
+   * @param fovy Field of view in y-direction
+   * @param aspect Aspect ratio between width and height
+   * @param near Camera-space distance to near plane
+   * @param far Camera-space distance to far plane
+   * @return The resulting matrix
+   */
+  static perspective(fovy: number, aspect: number, near: number, far: number): Matrix {
+    let top = near * Math.tan(fovy * Math.PI / 360);
+    let right = top * aspect;
+    return Matrix.frustum(-right, right, -top, top, near, far);
+  }
+
+  /**
+   * Returns the identity matrix
+   * @return A new identity matrix
+   */
+  static identity(): Matrix {
+    return new Matrix([
+      1, 0, 0, 0,
+      0, 1, 0, 0,
+      0, 0, 1, 0,
+      0, 0, 0, 1
+    ]);
+  }
+
+  /**
+   * Matrix multiplication
+   * @param other The matrix to multiply with
+   * @return The result of the multiplication this*other
+   */
+  mul(other: Matrix): Matrix {
+    let result = new Matrix([]);
+    for (let row = 0; row < 4; row++) {
+      for (let col = 0; col < 4; col++) {
+        let sum = 0;
+        for (let i = 0; i < 4; i++) {
+          sum += this.getVal(row, i) * other.getVal(i, col);
+        }
+        result.setVal(row, col, sum);
+      }
+    }
+    return result;
+  }
+
+  /**
+   * Matrix-vector multiplication
+   * @param other The vector to multiply with
+   * @return The result of the multiplication this*other
+   */
+  mulVec(other: Vector): Vector {
+    let result = new Vector(0, 0, 0, 0);
+    for (let row = 0; row < 4; row++) {
+      let sum = 0;
+      for (let col = 0; col < 4; col++) {
+        sum += this.getVal(row, col) * other.data[col];
+      }
+      result.data[row] = sum;
+    }
+    return result;
+  }
+
+  /**
+   * Returns the transpose of this matrix
+   * @return A new matrix that is the transposed of this
+   */
+  transpose(): Matrix {
+    let result = new Matrix([]);
+    for (let row = 0; row < 4; row++) {
+      for (let col = 0; col < 4; col++) {
+        result.setVal(col, row, this.getVal(row, col));
+      }
+    }
+    return result;
+  }
+
+  /**
+   * Debug print to console
+   */
+  print() {
+    for (let row = 0; row < 4; row++) {
+      console.log("> " + this.getVal(row, 0) +
+        "\t" + this.getVal(row, 1) +
+        "\t" + this.getVal(row, 2) +
+        "\t" + this.getVal(row, 3)
+      );
+    }
+  }
+}
---
+++ src/vector.ts
@@ -0,0 +1,240 @@
+/**
+ * Class representing a vector in 4D space
+ */
+ export default class Vector {
+    /**
+     * The variable to hold the vector data
+     */
+    data: [number, number, number, number];
+  
+    /**
+     * Create a vector
+     * @param x The x component
+     * @param y The y component
+     * @param z The z component
+     * @param w The w component
+     */
+    constructor(x: number, y: number, z: number, w: number) {
+      this.data = [x, y, z, w];
+    }
+  
+    /**no
+     * Returns the x component of the vector
+     * @return The x component of the vector
+     */
+    get x(): number {
+      return this.data[0];
+    }
+  
+    /**
+     * Sets the x component of the vector to val
+     * @param val - The new value
+     */
+    set x(val: number) {
+      this.data[0] = val;
+    }
+  
+    /**
+     * Returns the first component of the vector
+     * @return The first component of the vector
+     */
+    get r(): number {
+      return this.data[0];
+    }
+  
+    /**
+     * Sets the first component of the vector to val
+     * @param val The new value
+     */
+    set r(val: number) {
+      this.data[0] = val;
+    }
+  
+    /**
+     * Returns the y component of the vector
+     * @return The y component of the vector
+     */
+    get y(): number {
+      return this.data[1];
+    }
+  
+    /**
+     * Sets the y component of the vector to val
+     * @param val The new value
+     */
+    set y(val: number) {
+      this.data[1] = val;
+    }
+  
+    /**
+     * Returns the second component of the vector
+     * @return The second component of the vector
+     */
+    get g(): number {
+      return this.data[1];
+    }
+  
+    /**
+     * Sets the second component of the vector to val
+     * @param val The new value
+     */
+    set g(val: number) {
+      this.data[1] = val;
+    }
+  
+    /**
+     * Returns the z component of the vector
+     * @return The z component of the vector
+     */
+    get z(): number {
+      return this.data[2];
+    }
+  
+    /**
+     * Sets the z component of the vector to val
+     * @param val The new value
+     */
+    set z(val: number) {
+      this.data[2] = val;
+    }
+  
+    /**
+     * Returns the third component of the vector
+     * @return The third component of the vector
+     */
+    get b(): number {
+      return this.data[2];
+    }
+  
+    /**
+     * Sets the third component of the vector to val
+     * @param val The new value
+     */
+    set b(val: number) {
+      this.data[2] = val;
+    }
+  
+    /**
+     * Returns the w component of the vector
+     * @return The w component of the vector
+     */
+    get w(): number {
+      return this.data[3];
+    }
+  
+    /**
+     * Sets the w component of the vector to val
+     * @param val The new value
+     */
+    set w(val: number) {
+      this.data[3] = val;
+    }
+  
+    /**
+     * Returns the fourth component of the vector
+     * @return The fourth component of the vector
+     */
+    get a(): number {
+      return this.data[3];
+    }
+  
+    /**
+     * Sets the fourth component of the vector to val
+     * @param val The new value
+     */
+    set a(val: number) {
+      this.data[3] = val;
+    }
+  
+    /**
+     * Creates a new vector with the vector added
+     * @param other The vector to add
+     * @return The new vector;
+     */
+    add(other: Vector): Vector {
+      return new Vector(this.data[0] + other.x, this.data[1] + other.y, this.data[2] + other.z, this.data[3] + other.w);
+    }
+  
+    /**
+     * Creates a new vector with the vector subtracted
+     * @param other The vector to subtract
+     * @return The new vector
+     */
+    sub(other: Vector): Vector {
+      return new Vector(this.data[0] - other.x, this.data[1] - other.y, this.data[2] - other.z, this.data[3] - other.w);
+    }
+  
+    /**
+     * Creates a new vector with the scalar multiplied
+     * @param other The scalar to multiply
+     * @return The new vector
+     */
+    mul(other: number): Vector {
+      return new Vector(this.data[0] * other, this.data[1] * other, this.data[2] * other, this.data[3] * other);
+    }
+  
+    /**
+     * Creates a new vector with the scalar divided
+     * @param other The scalar to divide
+     * @return The new vector
+     */
+    div(other: number): Vector {
+      return new Vector(this.data[0] / other, this.data[1] / other, this.data[2] / other, this.data[3] / other);
+    }
+  
+    /**
+     * Dot product
+     * @param other The vector to calculate the dot product with
+     * @return The result of the dot product
+     */
+    dot(other: Vector): number {
+      return this.data[0] * other.x + this.data[1] * other.y + this.data[2] * other.z + this.data[3] * other.w;
+    }
+  
+    /**
+     * Cross product
+     * Calculates the cross product using the first three components
+     * @param other The vector to calculate the cross product with
+     * @return The result of the cross product as new Vector
+     */
+    cross(other: Vector): Vector {
+      return new Vector(this.data[1] * other.z - this.data[2] * other.y, this.data[2] * other.x - this.data[0] * other.z, this.data[0] * other.y - this.data[1] * other.x, 0);
+    }
+  
+    /**
+     * Returns an array representation of the vector
+     * @return An array representation.
+     */
+    valueOf(): [number, number, number, number] {
+      return this.data;
+    }
+  
+    /**
+     * Normalizes this vector in place
+     * @returns this vector for easier function chaining
+     */
+    normalize(): Vector {
+      const length = this.length;
+      return new Vector(this.data[0] / length, this.data[1] / length, this.data[2] / length, this.data[3] / length);
+    }
+  
+    /**
+     * Compares the vector to another
+     * @param other The vector to compare to.
+     * @return True if the vectors carry equal numbers. The fourth element may be both equivalent to undefined to still return true.
+     */
+    equals(other: Vector): boolean {
+      // return this.data[0] - other.x <= Number.EPSILON && this.data[1] - other.y <= Number.EPSILON && this.data[2] - other.z <= Number.EPSILON;
+      return this.data[0] === other.x && this.data[1] === other.y && this.data[2] === other.z;
+    }
+  
+    /**
+     * Calculates the length of the vector
+     * @return The length of the vector
+     */
+    get length(): number {
+      return Math.sqrt(this.data[0] * this.data[0] + this.data[1] * this.data[1] + this.data[2] * this.data[2] + this.data[3] * this.data[3]);
+    }
+  }
+  
+  
