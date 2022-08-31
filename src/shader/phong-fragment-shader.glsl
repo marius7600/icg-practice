b06eabb945ba6686443b25ed6a01986183429e13
JuliArzb <juliusarzberger@web.de>
2022-08-31 10:47:57 +0200

    Phong shader

--- src/shader/phong-fragment-shader.glsl
+++ src/shader/phong-fragment-shader.glsl
@@ -1,7 +1,36 @@
 precision mediump float;
 
-varying vec4 v_color; // Color of object
+// Receive color and position values
+varying vec3 v_color;
+varying vec3 v_position;
+varying vec3 v_normal;
+
+
+
+const vec3 lightPos = vec3(1.0, 1.0, 1.0);
+const float shininess = 16.0;
+const float kA = 0.3;
+const float kD = 0.6;
+const float kS = 0.7;
 
 void main(void) {
-gl_FragColor = v_color;
+  gl_FragColor = vec4(0.0, 0.0, 0.5, 1.0);
+  // Phong lighting calculation
+  //TODO
+  vec3 lightDir = normalize(lightPos - v_position);
+  vec3 viewDir = normalize(vec3(0.0, 0.0, 0.0) - v_position);
+  vec3 reflectDir = normalize(reflect(-lightDir, v_normal));
+
+  vec3 ambient = kA * v_color;
+
+  vec3 specularTerm = normalize(v_normal*(dot(v_normal, lightDir)*2.0 - lightDir)); //    const r = n.mul(2*n.dot(s)).sub(s);
+  vec3 specular = v_color * pow(max(dot(specularTerm, viewDir), 0.0), shininess); //Works
+
+  vec3 diffuse = v_color * max(dot(lightDir, v_normal), 0.0); //Works
+  specular = kS * specular;
+  diffuse = kD * diffuse;
+  gl_FragColor = vec4(ambient + diffuse + specular, 1.0);
+
+
+
 }
--- src/shader/phong-vertex-perspective-shader.glsl
+++ src/shader/phong-vertex-perspective-shader.glsl
@@ -1,14 +1,35 @@
-attribute vec3 a_position;
-attribute vec4 a_color;
+precision mediump float;
 
-varying vec4 v_color;
+attribute vec3 a_position;
+attribute vec3 a_normal;
+
+// Pass color as attribute and forward it
+// to the fragment shader
+// TODO
+attribute vec3 a_color;
+varying vec3 v_color;
+
 
 uniform mat4 M;
 uniform mat4 V;
 uniform mat4 P;
 uniform mat4 N; // normal matrix
 
+varying vec3 v_normal;
+
+// Pass the vertex position in view space
+// to the fragment shader
+// TODO
+varying vec3 v_position;
+
+
+
 void main() {
   gl_Position = P * V * M * vec4(a_position, 1.0);
+  
+  // Pass the color and transformed vertex position through
+  // TODO
   v_color = a_color;
+  v_position = vec3(M * vec4(a_position, 1.0));
+  v_normal = normalize((V * N * vec4(a_normal, 0)).xyz);
 }
