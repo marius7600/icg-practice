a368d40030d6866a4f6b18a2c96f61f290bbc92a
Marius Röhm <marius.roehm@gmail.com>
2023-01-31 20:56:46 +0100

    reformat

--- src/raytracer/rayvisitor.ts
+++ src/raytracer/rayvisitor.ts
@@ -1,17 +1,21 @@
-import Matrix from '../matrix';
-import Vector from '../vector';
-import Sphere from './ray-sphere';
-import Intersection from './intersection';
-import Ray from './ray';
-import Visitor from '../visitor';
-import phong from './phong';
+import Matrix from "../matrix";
+import Vector from "../vector";
+import Sphere from "./ray-sphere";
+import Intersection from "./intersection";
+import Ray from "./ray";
+import Visitor from "../visitor";
+import phong from "./phong";
 import {
-  Node, GroupNode, SphereNode,
-  AABoxNode, TextureBoxNode, CameraNode, LightNode
-} from '../nodes';
-import AABox from './aabox';
-import PhongProperties from '../phong-properties';
-
+  Node,
+  GroupNode,
+  SphereNode,
+  AABoxNode,
+  TextureBoxNode,
+  CameraNode,
+  LightNode,
+} from "../nodes";
+import AABox from "./aabox";
+import PhongProperties from "../phong-properties";
 
 interface Intersectable {
   intersect(ray: Ray): Intersection | null;
@@ -28,9 +32,8 @@ export default class RayVisitor implements Visitor {
    * set individual pixels
    */
   imageData: ImageData;
-  camera: CameraNode;   // TODO do we need this? Camera is alwas given with render function
+  camera: CameraNode; // TODO do we need this? Camera is alwas given with render function
   objects: Array<Intersectable>;
-
 
   // TODO declare instance variables here
   // stack:  [{ matrix: Matrix, inverse: Matrix }];
@@ -38,7 +41,6 @@ export default class RayVisitor implements Visitor {
   intersection: Intersection | null;
   intersectionColor: Vector;
   lightNodes: Array<LightNode> = [];
-
 
   /**
    * Creates a new RayVisitor
@@ -57,7 +59,9 @@ export default class RayVisitor implements Visitor {
   }
 
   visitLightNode(node: LightNode): void {
+    // Get the transform of the current node
     let myPosition = this.stack[this.stack.length - 1].mul(node.position);
+    // Add a light node to the list of light nodes
     this.lightNodes.push(new LightNode(node.color, myPosition));
   }
 
@@ -92,24 +96,32 @@ export default class RayVisitor implements Visitor {
         let minIntersection = new Intersection(Infinity, null, null);
         let minObj = null; // minObj is the closest object to the camera
 
-        for (let shape of this.objects) { //This.objects wird in der Visit Methode gefüllt (enhält sphere und aabox)
+        for (let shape of this.objects) {
+          //This.objects wird in der Visit Methode gefüllt (enhält sphere und aabox)
           const intersection = shape.intersect(ray);
-          if (intersection && intersection.closerThan(minIntersection)) { // if intersection != null
+          if (intersection && intersection.closerThan(minIntersection)) {
+            // if intersection != null
             minIntersection = intersection;
             minObj = shape;
           }
         }
 
         if (minObj) {
-
-          if (!minObj.color) { // if no color is given then set pixel to black , else calculate color with phong
-            data[4 * (width * y + x) + 0] = 255;
+          if (!minObj.color) {
+            // if no color is given then set pixel to black , else calculate color with phong
+            data[4 * (width * y + x) + 0] = 0;
             data[4 * (width * y + x) + 1] = 0;
             data[4 * (width * y + x) + 2] = 0;
             data[4 * (width * y + x) + 3] = 255;
           } else {
             // let color = phong(minObj.color, minIntersection, this.lightNodes, camera.eye, phongProperties);
-            let color = phong(minObj.color, minIntersection, this.lightNodes, this.camera.center, phongProperties);
+            let color = phong(
+              minObj.color,
+              minIntersection,
+              this.lightNodes,
+              this.camera.center,
+              phongProperties
+            );
 
             // Tino
             // let allLightNodes: Array<LightNode> = new Array();
@@ -124,7 +136,7 @@ export default class RayVisitor implements Visitor {
         }
       }
     }
-    this.context.putImageData(this.imageData, 0, 0); // put the image data to the context (canvas) 
+    this.context.putImageData(this.imageData, 0, 0); // put the image data to the context (canvas)
   }
 
   /**
@@ -133,7 +145,9 @@ export default class RayVisitor implements Visitor {
    */
   visitGroupNode(node: GroupNode) {
     // TODO traverse the graph and build the model matrix
-    let toWorld = this.stack.at(this.stack.length - 1).mul(node.transform.getMatrix());
+    let toWorld = this.stack
+      .at(this.stack.length - 1)
+      .mul(node.transform.getMatrix());
     // this.stack.push({ matrix: wiggse, inverse: wiggse.inverse() });
     // this.stack.push({ matrix: toWorld, inverse: toWorld.transpose() });   // TODO is this correct?
     this.stack.push(toWorld);
@@ -148,7 +162,6 @@ export default class RayVisitor implements Visitor {
    * @param node - The node to visit
    */
   visitSphereNode(node: SphereNode) {
-
     let m = this.stack[this.stack.length - 1]; //translation matrix
     // console.log(m);
     // console.log(node.radius)
@@ -160,8 +173,9 @@ export default class RayVisitor implements Visitor {
 
     let scale = Math.sqrt(xScale * xScale + yScale * yScale + zScale * zScale);
     // console.log(scale);
-    this.objects.push(new Sphere(m.mul(node.center),
-      node.radius * scale, node.color));
+    this.objects.push(
+      new Sphere(m.mul(node.center), node.radius * scale, node.color)
+    );
   }
 
   /**
@@ -174,13 +188,15 @@ export default class RayVisitor implements Visitor {
     this.stack.push(Matrix.identity());
     mat = this.stack[this.stack.length - 1].mul(mat);
 
-    this.objects.push(new AABox(
-      // mat.mulVec(new Vector(-0.5, -0.5, -0.5, 1)),
-      mat.mul(new Vector(-0.5, -0.5, -0.5, 1)),
-      // mat.mulVec(new Vector(0.5, 0.5, 0.5, 1)),
-      mat.mul(new Vector(0.5, 0.5, 0.5, 1)),
-      node.color
-    ));
+    this.objects.push(
+      new AABox(
+        // mat.mulVec(new Vector(-0.5, -0.5, -0.5, 1)),
+        mat.mul(new Vector(-0.5, -0.5, -0.5, 1)),
+        // mat.mulVec(new Vector(0.5, 0.5, 0.5, 1)),
+        mat.mul(new Vector(0.5, 0.5, 0.5, 1)),
+        node.color
+      )
+    );
   }
 
   /**
@@ -196,18 +212,22 @@ export default class RayVisitor implements Visitor {
     eye.z -= 2;
     eye = this.stack[this.stack.length - 1].mul(node.eye);
     let up = this.stack[this.stack.length - 1].mul(node.up);
-    this.camera = new CameraNode(node.eye, node.center, node.up, node.fovy, node.aspect,
-      node.near, node.far);
+    this.camera = new CameraNode(
+      node.eye,
+      node.center,
+      node.up,
+      node.fovy,
+      node.aspect,
+      node.near,
+      node.far
+    );
     // coordinaten hardcoden für test der camera
     // this.camera = new CameraNode(new Vector(0, 0, 0, 1), new Vector(0, 0, -1, 1), new Vector(0, 1, 0, 0), node.fovy, node.aspect, node.near, node.far);
   }
 
   /**
- * Visits a camera node
- * @param node The node to visit
- */
-  visitGroupNodeCamera(node: GroupNode) {
-
-  }
+   * Visits a camera node
+   * @param node The node to visit
+   */
+  visitGroupNodeCamera(node: GroupNode) { }
 }
-
